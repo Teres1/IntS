cb7ce5f0140c754569144480284dd6ff5b08bb0c
Slavi <slavi_14@abv.bg>
2019-06-15 06:31:58 +0300

    training

--- IntS/src/general_math_tasks/LargestNumber.java
+++ IntS/src/general_math_tasks/LargestNumber.java
@@ -17,13 +17,13 @@ public class LargestNumber {
         System.out.println(ln.largestNumber(arr));
     }
 
-    private String largestNumber(int[] nums) {
-        if (nums == null || nums.length == 0) {
+    private String largestNumber(int[] arr) {
+        if (arr == null || arr.length == 0) {
             return "";
         }
-        String[] s = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            s[i] = nums[i] + "";
+        String[] s = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            s[i] = arr[i] + "";
         }
         Arrays.sort(s, (String s1, String s2) -> (s2 + s1).compareTo(s1 + s2));
         return Arrays.stream(s).reduce((x, y) -> x.equals("0") ? y : x + y).get();
